e8edeb7545d4ff6b6a8c89ba3e30e63f058a4d62
Allan Kerr <personal.allankerr@gmail.com>
2016-06-10 10:54:00 -0600

    Initial commit

---
+++ GoogleMapsExtension/GoogleMapsExtension.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMapsExtension.h
+//  GoogleMapsExtension
+//
+//  Created by Allan Kerr on 2016-06-10.
+//  Copyright © 2016 Allan Kerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleMapsExtension.
+FOUNDATION_EXPORT double GoogleMapsExtensionVersionNumber;
+
+//! Project version string for GoogleMapsExtension.
+FOUNDATION_EXPORT const unsigned char GoogleMapsExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapsExtension/PublicHeader.h>
+
+
